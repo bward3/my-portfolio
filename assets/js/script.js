b520893e90c9e0def485c81b5de4972482a3440d
Brian Ward <brianwardcs@gmail.com>
2022-04-15 15:13:59 -0600

    updated portfolio, added resume, recent works, links

---
+++ assets/js/script.js
@@ -0,0 +1,12 @@
+var weatherImg = document.getElementById('weather');
+weatherImg.onclick = function() {
+    window.location = "https://bward3.github.io/weather-dashboard/"
+}
+var plannerImg = document.getElementById('planner');
+plannerImg.onclick = function() {
+    window.location = "https://bward3.github.io/day-planner/"
+}
+var quizImg = document.getElementById('quiz');
+quizImg.onclick = function() {
+    window.location = "https://bward3.github.io/coding-quiz/"
+}
