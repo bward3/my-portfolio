7590b3c15281c080bfa026e3ae60bdaaf816a9cd
Brian Ward <brianwardcs@gmail.com>
2022-04-16 14:08:18 -0600

    added modals with screenshots/ links to projects

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,12 +1,29 @@
 var weatherImg = document.getElementById('weather');
+var modal = document.getElementById('myModal');
+var modalImg = document.getElementById('modal-img');
+var modalLink = document.getElementById('modal-link');
+
 weatherImg.onclick = function() {
-    window.location = "https://bward3.github.io/weather-dashboard/"
+    modal.style.display = 'block';
+    modalImg.setAttribute('src', './assets/images/weather-sc.png');
+    modalLink.setAttribute('href', 'https://bward3.github.io/weather-dashboard/')
 }
 var plannerImg = document.getElementById('planner');
 plannerImg.onclick = function() {
-    window.location = "https://bward3.github.io/day-planner/"
+    modal.style.display = 'block';
+    modalImg.setAttribute('src', './assets/images/planner-sc.png');
+    modalLink.setAttribute('href', 'https://bward3.github.io/day-planner/')
 }
 var quizImg = document.getElementById('quiz');
 quizImg.onclick = function() {
-    window.location = "https://bward3.github.io/coding-quiz/"
+    modal.style.display = 'block';
+    modalImg.setAttribute('src', './assets/images/quiz-sc.png');
+    modalLink.setAttribute('href', 'https://bward3.github.io/coding-quiz/')
+}
+
+
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
 }
